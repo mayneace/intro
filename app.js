d84a531b448dc98e6c8f3a68a495533976bc40cf
mayneace <mosesadebayour@gmail.com>
2026-04-23 12:18:36 +0100

    second

--- app.js
+++ app.js
@@ -30,9 +30,11 @@ hamburg.addEventListener("click", () => {
   hamburg.classList.toggle("opacity-0");
 });
 
-feature.addEventListener("click", () => {
+feature.addEventListener("click", (event) => {
   fDrop.classList.toggle("hidden");
   arrow.classList.toggle("rotate-180");
+
+  event.preventDefault();
 });
 
 featuree.addEventListener("click", (event) => {
@@ -42,9 +44,11 @@ featuree.addEventListener("click", (event) => {
   event.preventDefault();
 });
 
-compa.addEventListener("click", () => {
+compa.addEventListener("click", (event) => {
   cDrop.classList.toggle("hidden");
   arrowedd.classList.toggle("rotate-180");
+
+  event.preventDefault();
 });
 
 compaa.addEventListener("click", (event) => {
@@ -53,3 +57,10 @@ compaa.addEventListener("click", (event) => {
 
   event.preventDefault();
 });
+
+//  git init
+//  git add .
+//   git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/mayneace/intro.git
+//  git push -u origin main
